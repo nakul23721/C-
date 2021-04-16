8db9e33ade96d3ef1efdcf371278f61d17724e0e
Nakul Verma <nakulverma2312@gmail.com>
2021-04-16 23:45:22 +0530

    Fibonacci Program

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     int n, t1 = 0, t2 = 1, nextTerm = 0;
 
     cout << "Enter the number of terms: ";
@@ -9,13 +10,16 @@ int main() {
 
     cout << "Fibonacci Series: ";
 
-    for (int i = 1; i <= n; ++i) {
-        // Prints the first two terms.
-        if(i == 1) {
+    for (int i = 1; i <= n; ++i) 
+    {
+        
+        if(i == 1) 
+        {
             cout << t1 << ", ";
             continue;
         }
-        if(i == 2) {
+        if(i == 2) 
+        {
             cout << t2 << ", ";
             continue;
         }
