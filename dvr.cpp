38c3af6eee33bf3676c31276f23d6f867e95ecb8
Nakul Verma <nakulverma2312@gmail.com>
2021-05-10 23:47:28 +0530

    DVR Program

--- dvr.cpp
+++ dvr.cpp
@@ -11,7 +11,7 @@ int main()
     int nodes,i,j,k,count=0;
 printf("\nEnter the number of nodes : "); 
 scanf("%d",&nodes);
-printf("\nEnter the cost matrix :\n"); 
+printf("\nEnter the cost-matrix :\n"); 
 for(i=0;i<nodes;i++)
 {
 for(j=0;j<nodes;j++)
